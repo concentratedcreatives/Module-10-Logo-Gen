4ff60cfc585980977244ac66102e9d5e1045a482
Blaze Harmon <96632846+concentratedcreatives@users.noreply.github.com>
2023-12-23 20:23:49 -0500

    commiting changes via upload due to repo issues
    had issues in my terminal

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const {Triangle, Square, Circle } = require('./library/shapes.js');
+const shapes = ['Triangle', 'Square', 'Circle'];
+
+const composeImage = ({ text, characterColor, shape }) => `
+<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  <rect width="100%" height="100%" fill="white" />
+  ${shape}
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${characterColor}">${text}</text>
+</svg>
+`;
+
+
+inquirer.prompt([
+  { type: 'input', message: 'Logo text (up to 3 characters): ', name: 'text' },
+  { type: 'input', message: 'Text color (keyword or hexcode): ', name: 'characterColor' },
+  { type: 'list', message: 'Select a shape: ', name: 'shape', choices: shapes },
+  { type: 'input', message: 'Shape color (keyword or hexcode): ', name: 'shapeColor' }
+]).then(({ shape, ...answers }) => {
+  const selectedShape = new (eval(shape))(); 
+  selectedShape.setColor(answers.shapeColor);
+  const image = composeImage({ ...answers, shape: selectedShape.render() });
+  const filePath = './samples/logo.svg';
+  fs.writeFile(filePath, image, (err) =>
+    err ? console.error(err) : console.log(`Generated logo.svg`)
+  );
+});
